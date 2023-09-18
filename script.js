1b522df359dad71cb730fc7f6a57e8bbcd44030f
FredCODEUR <119888976+FredCODEUR@users.noreply.github.com>
2023-09-18 21:23:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var screenWidth = function(){
-    if (window.innerWidth <= 768){
+    if (window.innerWidth <= 1024){
         console.log(window.innerWidth);
         return window.innerWidth;
     }
@@ -8,7 +8,7 @@ var screenWidth = function(){
 
 $(function(){
     $(".navbar").click(function(){
-        if(window.innerWidth <= 768){
+        if(window.innerWidth <= 1024){
             $(".line-burger-1").toggleClass("cross-burger-1")
             ,
             $(".line-burger-2").toggleClass("cross-burger-2")
@@ -19,7 +19,7 @@ $(function(){
         }
     });
     $(".navbar").hover(function(){
-        if(window.innerWidth > 768){
+        if(window.innerWidth > 1024){
             $(".line-burger-1").toggleClass("cross-burger-1")
             ,
             $(".line-burger-2").toggleClass("cross-burger-2")
@@ -28,6 +28,17 @@ $(function(){
             ,
             $(".dropdown-menu").toggleClass("sidebar")
         }
+    });
+    $(".society").hover(function(){
+        $(".name h1").toggleClass("selection")
+        ,
+        $(".name h3").toggleClass("selection")
+    });
+    $(".specialities a").hover(function(){
+        $(this).addClass("selection");
+        },
+        function(){
+            $(this).removeClass("selection");
     });
 });
 
